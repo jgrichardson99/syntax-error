99f05007d351e95c65419c57e077181dd7c566e8
Richard H <47335985+Equailax@users.noreply.github.com>
2024-12-08 16:46:40 -0700

    TourInfo.tsx displays info from database

--- backend/index.ts
+++ backend/index.ts
@@ -8,20 +8,24 @@ const db = mysql.createConnection({
     host:"localhost",
     user:"root",
     password:"password",
-    database:"syntax-error"
+    database:"syntax-error",
+    multipleStatements: true
 })
 
 app.use(express.json());
 app.use(cors());
 
+// Homepage
 app.get("/", (req, res) => {
     res.json("Hello this is the backend")
 })
 
+// Tours Page
 app.get("/tour", (req, res) => {
-    const q = "SELECT * FROM tour";
+    const q = "SELECT * FROM tour; SELECT * FROM tour_dates; SELECT * FROM tour_guests";
     db.query(q, (err, data) => {
         if (err) return res.json(err);
+        console.log(data)
         return res.json(data);
     })
 })
--- client/src/components/TourInfo.tsx
+++ client/src/components/TourInfo.tsx
@@ -4,9 +4,67 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function TourInfo() {
+  // Below section (until before the return) will grab data from the database
+  // It does a GET on the link in the bracket which is more detailed in syntax-error/backend/index.ts, where 8800 = backend listener port, and /tour is a subpage that would issue the query from the database
+  const [tourData, setTourData] = useState([]);
+  const [tourDatesData, setTourDateData] = useState([]);
+  const [tourGuestsData, setTourGuestsData] = useState([]);
+
+  useEffect(() => {
+    const fetchAllTours = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/tour");
+        setTourData(res.data[0]);
+        setTourDateData(res.data[1]);
+        setTourGuestsData(res.data[2]);
+      } catch(err) {
+        console.log(err);
+      }
+    }
+    fetchAllTours();
+  }, [])
+
+
+  // Return true/false if the strings are the same
+  function sameTour(tour1: string, tour2: string) {
+    if (tour1===tour2) {
+      return true;
+    }
+    return false;
+  }
+
+  // Trim off the timezone at the end of date
+  function trimDate(date: string) {
+    return date.slice(0,10)
+  }
+
+
   return (
     <div className="list-group">
-      <a
+      {tourData.map(tour => (
+            <div className="tours" key={tour.Name}>
+              <details>
+                <summary>
+                  {tourGuestsData.map(tourGuests => (
+                    <h5>
+                      {tour.Name===tourGuests.Name ? "Syntax Error's " + tour.Name + " with " + tourGuests.Special_guests : "Syntax Error's " + tour.Name}
+                    </h5>
+                  ))}
+                  <h5>
+                    {/* Syntax Error's {tour.Name} */}
+                    {/* Syntax Error's {tour.Name} with  @ {tour.Location} */}
+                  </h5>
+                </summary>
+                    {tourDatesData.map(tourDates => (
+                      <small>
+                        {tour.Name===tourDates.Name ? (trimDate(tourDates.Date) + " @ " + tourDates.Location) : ""} {tour.Name===tourDates.Name ? <br /> : ""}
+                      </small>
+                    ))}
+              </details>
+            </div>
+        ))}
+
+      {/* <a
         href="#"
         className="list-group-item list-group-item-action "
         aria-current="true"
@@ -54,7 +112,7 @@ function TourInfo() {
         </div>
         <p className="mb-1">Venue Name: Venue Address</p>
         <small className="text-body-secondary">January 11th, 2030 @ 7PM.</small>
-      </a>
+      </a> */}
     </div>
   );
 }
